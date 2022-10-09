43b98d30463414bba590c78fa4e96088ec516d00
alekkkkonrad <alexandrekonradmain@gmail.com>
2022-10-09 03:49:54 -0300

    att de pedidos

--- middlewares/pedidoValidation.js
+++ middlewares/pedidoValidation.js
@@ -9,7 +9,18 @@ const pedidoInsertValidation = () => {
         .isString()
         .withMessage("O título é obrigatório")
         .isLength({min: 3})
-        .withMessage("o título precisa de mais de 3 caracteres"),
+        .withMessage("o título precisa ter mais de 3 caracteres"),
+        body("local")
+        .not()
+        .equals("undefined")
+        .withMessage("O local de doação é obrigatório")
+        .isString()
+        .withMessage("O local de doação é obrigatório")
+        .isLength({min: 5})
+        .withMessage("O local de doação contém mais de de 5 caracteres"),
+        body("tipos")
+        .isLength({min: 1})
+        .withMessage("Insira ao menos um tipo sanguíneo para a doação"),
     ]
 }
 
